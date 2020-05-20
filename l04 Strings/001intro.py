601c9fb00be22affd49ffe79e5d23ebb2c4c1275
Washington Mugo <washiemugo@gmail.com>
2020-05-20 14:51:50 +0300

    polishing on strings

---
+++ l04 Strings/001intro.py
@@ -0,0 +1,33 @@
+# you can use both singl or double qutes
+print("Hello, world!")
+print('Hello, World!')
+
+#seting variable on a string
+welcome_message = "Hello, World !"
+print("Message : ", welcome_message)
+
+#Whenever you need to use quoatation marks 
+# use single quotes for string 
+pet_halibut = 'Why should i be tarred with the epithet "loony" merely because i have a pet habithat?'
+print(pet_halibut)
+
+#for a string that uses both single quote and double quote 
+# u can use a \ (backslash to escape quotes)
+salesman = '"I think you\'re an encyclopedia salesman"'
+print(salesman)
+
+#combining and repeating strings
+first_word = "Hello"
+second_word = "there"
+print(first_word +" "+ second_word)
+
+#repeat strings 
+word = "Hello"
+print((word+" ")*5)
+
+#length of string (Number of characters in a string)
+udacity_length = len("Udacity")
+print(udacity_length)
+
+#indexing
+print(first_word[0])
---
+++ l04 Strings/002quiz.py
@@ -0,0 +1,22 @@
+#Quiz: Write a Server Log Message
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
+
+# TODO: print a log message using the variables above.
+# The message should have the same format as this one:
+# "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
+print(username +" accessed the site " + url + " at"+" "+timestamp)
+
+#Quiz: len()
+given_name = "William"
+middle_names = "Bradley"
+family_name = "Pitt"
+
+name_length = len(given_name+" "+middle_names+" "+family_name)
+#todo: calculate how long this name is
+# Now we check to make sure that the name fits within the driving license character limit
+# # Nothing you need to do here
+driving_license_character_limit = 28
+print(name_length <= driving_license_character_limit)
+print(name_length)
