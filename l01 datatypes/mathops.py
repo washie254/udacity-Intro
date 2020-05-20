7dc114765b91522fde1af0b323ba77d9f52b0a69
Washington Mugo <washiemugo@gmail.com>
2020-05-20 07:55:51 +0300

    Math operators

---
+++ l01 datatypes/mathops.py
@@ -0,0 +1,23 @@
+###
+# /     -> division
+# //    -> divides and rounds down to the nearest integer
+# %     -> mod (the remainder after dividing )
+# **    -> Exponential 
+# ^     -> Bitwise XOR
+###
+
+print("DIVISION: ", 9/2)
+print("INT DIV : ", 9//2)
+print("MODULO  : ", 9%2)
+print("EXPONENT: ", 9**2)
+
+#QO1
+# Write an expression that calculates the average of 23, 32 and 64
+# Place the expression in this print statement
+print((23+32+64)/3)
+
+#QO2
+# Fill this in with an expression that calculates how many tiles are needed.
+print((9*7) + (5*7))
+# Fill this in with an expression that calculates how many tiles will be left over.
+print((17*6)-((9*7) + (5*7)))
