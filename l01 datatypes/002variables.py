eb072fb17dff7e6fb5bc61af7e5911a6cfdf47c9
Washington Mugo <washiemugo@gmail.com>
2020-05-20 08:49:52 +0300

    finalizing data types and math operations

---
+++ l01 datatypes/002variables.py
@@ -0,0 +1,16 @@
+#mountain view population 
+mv_population = 74728
+mv_area = 11.995
+mv_density = mv_population/mv_area
+print(mv_density)
+
+#assigning in one line 
+x, y, z = 2, 3, 5
+print("x=",x,"y=",y,"z=",z)
+
+###
+#more assignment operators
+#   Symbol      Example             Equivalent
+#   =           x=2                 x=2         
+#   +=          x+=2                x=x+2
+#   -=          x-=2                x=x-2
