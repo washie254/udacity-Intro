a56d034ad171ae5ca38705e5cde8f99d9237f5b9
Washington Mugo <washiemugo@gmail.com>
2020-05-20 18:03:52 +0300

    final Lesson one

---
+++ l08 summary/highlights.py
@@ -0,0 +1,28 @@
+#We covered four important data types that you'll use all the time in programming:
+## datatypes discussed so far
+#   int     eg          12
+#   float   eg          12.12
+#   bool    eg          True
+#   string  eg          "Raven"
+
+#OPERATORS COVERED
+###  COMPARISON OPERATORS  (Symbol operation)
+#   <       less than
+#   >       Greater than
+#   <=      less than or equal to
+#   >=      Greater than or equal to 
+#   ==      Equal to 
+#   !=      Not equal to 
+
+###   LOGICAL OPERATORS (Keyword operation)
+#   and         Evaluates if both sides are True
+#   or          Evaluates if at least one side is true 
+#   not         Inverse a boolean typ
+
+
+
+def multiply(a, b):
+  prd = a * b
+  print(prd)
+
+print(multiply(4,5))
