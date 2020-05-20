0025702106a78b4790ff099f00d94940e34f444f
Washington Mugo <washiemugo@gmail.com>
2020-05-20 18:12:06 +0300

    Lesson two higlights

---
+++ LESSON 2 DATA STRUCTURES/l01 intro/001intro.py
@@ -0,0 +1,11 @@
+#   coverred under Lesson 2
+# Types of Data Structures: 
+# ->  Lists, 
+# ->  Tuples,     
+# ->  Sets,    
+# ->  Dictionaries, 
+# ->  Compound Data Structures
+
+# last two tepes of operators
+#   -> Mmbership 
+#   -> identity
